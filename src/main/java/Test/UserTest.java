9f50d9e449d2552406ef658f790e7520739bf6d9
a13487849086 <39087195+a13487849086@users.noreply.github.com>
2019-12-19 16:07:09 +0800

    test

---
+++ src/main/java/Test/UserTest.java
@@ -0,0 +1,26 @@
+package Test;
+
+
+import com.cheirmin.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @Filename
+ * @auther 吴星辰;
+ * @data 2019/12/19 14:50;
+ * @Descripion
+ * @Version 1.1.1
+ * @Function
+ * @History
+ */
+
+
+public class UserTest {
+
+    @Autowired
+    UserService userService;
+
+
+
+
+}
