a35940e05a7ec2f5ce28f1c7746430fad8e4afd2
hy1079503225 <58418763+hy1079503225@users.noreply.github.com>
2019-12-23 11:47:50 +0800

    cart
    购物车

--- src/main/java/com/cheirmin/controller/vo/ShoppingCartItemVO.java
+++ src/main/java/com/cheirmin/controller/vo/ShoppingCartItemVO.java
@@ -1,6 +1,7 @@
 package com.cheirmin.controller.vo;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @Message:购物车页面购物项VO
@@ -9,4 +10,74 @@ import java.io.Serializable;
  * @Version 1.0
  */
 public class ShoppingCartItemVO implements Serializable {
+
+    private Long cartItemId;
+
+    private Long userId;
+
+    private Long bookId;
+
+    private String bookCoverImg;
+
+    private String bookName;
+
+    private Integer bookCount;
+
+    private Integer sellingPrice;
+
+    public Long getCartItemId() {
+        return cartItemId;
+    }
+
+    public void setCartItemId(Long cartItemId) {
+        this.cartItemId = cartItemId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookCoverImg() {
+        return bookCoverImg;
+    }
+
+    public void setBookCoverImg(String bookCoverImg) {
+        this.bookCoverImg = bookCoverImg;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public Integer getBookCount() {
+        return bookCount;
+    }
+
+    public void setBookCount(Integer bookCount) {
+        this.bookCount = bookCount;
+    }
+
+    public Integer getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(Integer sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
 }
--- src/main/java/com/cheirmin/service/ShoppingCartService.java
+++ src/main/java/com/cheirmin/service/ShoppingCartService.java
@@ -1,10 +1,41 @@
 package com.cheirmin.service;
 
+import com.cheirmin.controller.vo.ShoppingCartItemVO;
+import com.cheirmin.pojo.ShoppingCartItem;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
 /**
  * @Message:
  * @Author：Cheirmin
  * @Date: 2019/12/13 19:38
  * @Version 1.0
  */
+@Repository
 public interface ShoppingCartService {
+
+//    String putCart(Model model,Integer bookCount, Long bookId, HttpSession session);
+//
+//    String selectCart(Model model,HttpSession session);
+//
+//    String deleteBooks(Long bookId,HttpSession session);
+//
+//    String clear(HttpSession session);
+//
+//    String orderConfirm(Model model,HttpSession session);
+
+    String saveCartItem(ShoppingCartItem shoppingCartItem);
+
+    String updateCartItem(ShoppingCartItem shoppingCartItem);
+
+    ShoppingCartItem getCartItemByCartItemId(Long cartItemId);
+
+    Boolean deleteByCartItemId(Long cartItemId);
+
+    List<ShoppingCartItemVO> getMyShoppingCartItems(Long userId);
+
 }
